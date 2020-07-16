457fea3457f526c340726cd6ff5ae793506894e8
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2020-07-16 18:07:22 +0300

    Cherry multiple repetitions

--- scripts/cherry.sh
+++ scripts/cherry.sh
@@ -8,11 +8,14 @@ gamma=0.9746666325676951 # fico
 # gamma=0.42118863049095606 # fico_old
 # gamma=0.8580724846537919 # credit
 max_iter=20
+for s in {1..10}
+do
 for i in {0..4}
 # for i in {0..3} # fico_old
 do
 # python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_heur_data_${dataset}_a_${alpha[$i]} --alpha=${alpha[$i]} --njobs=1 --algo=gh
-python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_thres_data_${dataset}_a_${alpha[$i]} --alpha=${alpha[$i]} --njobs=1 --algo=th
-python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_data_${dataset}_a_${alpha[$i]} --alpha=${alpha[$i]} --njobs=1 --algo=it
-python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_parallel_data_${dataset}_a_${alpha[$i]} --alpha=${alpha[$i]} --njobs=$njobs --algo=it --max_iter=$max_iter
+python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_thres_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=1 --algo=th --seed=$s
+python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=1 --algo=it --seed=$s
+python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_parallel_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=$njobs --algo=it --max_iter=$max_iter --seed=$s
+done
 done
