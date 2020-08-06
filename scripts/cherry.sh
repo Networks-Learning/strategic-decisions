bf908792a02925439fee9094f7e9e15d69a8e063
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2020-08-06 14:11:26 +0300

    Single seed for cherry

--- scripts/cherry.sh
+++ scripts/cherry.sh
@@ -5,8 +5,9 @@ dataset=fico
 gamma=0.9746666325676951 # fico
 # gamma=0.8580724846537919 # credit
 # max_iter=20
-for s in {1..10}
-do
+s=1
+# for s in {1..10}
+# do
 for i in {0..4}
 do
 python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_thres_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=1 --algo=th --seed=$s
@@ -14,4 +15,4 @@ python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outpu
 python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_components_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=1 --algo=itc --seed=$s
 # python -m lib.real  --data=data/processed/$dataset --gamma=$gamma --output=outputs/cherry_iterative_parallel_data_${dataset}_a_${alpha[$i]}_seed_${s} --alpha=${alpha[$i]} --njobs=$njobs --algo=it --max_iter=$max_iter --seed=$s
 done
-done
+# done
